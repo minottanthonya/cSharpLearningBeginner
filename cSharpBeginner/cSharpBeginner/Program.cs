d31c6fb82c7c026198eb3804553593eb94cfaa13
Tony Minott <minottanthonya@gmail.com>
2018-08-10 15:23:26 -0400

    testing out some new console logs using variables

--- cSharpBeginner/cSharpBeginner/Program.cs
+++ cSharpBeginner/cSharpBeginner/Program.cs
@@ -10,19 +10,9 @@ namespace cSharpBeginner
     {
         static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Tony";
-            bool isWorking = true;
-
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            const float Pi = 3.14f;
         }
     }
 }
