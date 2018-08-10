d9bfecdc4ccf675d428e90627b3d9410e804c839
Tony Minott <minottanthonya@gmail.com>
2018-08-10 15:26:10 -0400

    This is the whole lesson

--- cSharpBeginner/cSharpBeginner/Program.cs
+++ cSharpBeginner/cSharpBeginner/Program.cs
@@ -10,6 +10,22 @@ namespace cSharpBeginner
     {
         static void Main(string[] args)
         {
+          //First Part//  
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Tony";
+            bool isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            //Second Part//
             Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
             Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
             const float Pi = 3.14f;
