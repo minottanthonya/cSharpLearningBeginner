fa6d29d16275505c29faeea6dbfe1a573e02171a
Tony Minott <minottanthonya@gmail.com>
2018-08-14 08:50:12 -0400

    Adding the proper comments and added the flag variable

--- cSharpBeginnerThree/cSharpBeginnerThree/Program.cs
+++ cSharpBeginnerThree/cSharpBeginnerThree/Program.cs
@@ -27,16 +27,23 @@ namespace cSharpBeginnerThree //CSharp Fundamentals
             // ARRAYS
             // Array of length 3. The length is fixed in C#.
 
-            int[] numbers = new int[3]; 
+            var numbers = new int[3] { 1, 2, 3 }; 
             numbers[0] = 1;
 
             Console.WriteLine(numbers[0]);
             Console.WriteLine(numbers[1]);  // Filled with default value based on array type.
             Console.WriteLine(numbers[2]);
 
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
             // Can define values at initialization
 
-            string[] names = new string[3] {"Jack", "John", "Mary"};
+            var names = new string[3] {"Jack", "John", "Mary"};
 
             // STRINGS
             
--- cSharpBeginnerThree/cSharpBeginnerThree/RgbColor.cs
+++ cSharpBeginnerThree/cSharpBeginnerThree/RgbColor.cs
@@ -1,5 +1,9 @@
 ﻿namespace cSharpBeginnerThree //CSharp Fundamentals
 {
+    //You will use a struct maybe 1% of the time, so the rest of the time you will use classes. 
+    //Structures are almost identical to classes. 
+    //You will only want to use a structure when you want to define a small lightweight object.
+
     public struct RgbColor
     {
         public int Red;
